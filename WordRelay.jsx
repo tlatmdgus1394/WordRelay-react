c91fe67a4138c85076b1730b8f46b9a5ae1a5371
SeungHyun Shim <tlatmdgus1394@gmail.com>
2022-03-25 19:30:17 +0900

    class

--- WordRelay.jsx
+++ WordRelay.jsx
@@ -3,11 +3,52 @@ const { Component } = React;
 
 class WordRelay extends Component {
   state = {
-    text: "hello webpack",
+    word: "제로초",
+    value: "",
+    result: "",
+  };
+
+  onSubmitForm = (e) => {
+    e.preventDefault();
+    if (this.state.word[this.state.word.length - 1] === this.state.value[0]) {
+      //정답
+      this.setState({
+        result: "딩동댕",
+        word: this.state.value,
+        value: "",
+      });
+    } else {
+      //오답
+      this.setState({
+        result: "땡",
+        value: "",
+      });
+    }
+    this.input.focus();
+  };
+  onChangeInput = (e) => {
+    this.setState({ value: e.target.value });
+  };
+  onRefInput = (c) => {
+    this.input = c;
   };
 
   render() {
-    return <h1>{this.state.text}</h1>;
+    return (
+      <>
+        <div>{this.state.word}</div>
+        <form onSubmit={this.onSubmitForm}>
+          <input
+            ref={this.onRefInput}
+            type="text"
+            value={this.state.value}
+            onChange={this.onChangeInput}
+          />
+          <button>입력!!!</button>
+        </form>
+        <div>{this.state.result}</div>
+      </>
+    );
   }
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const ReactRefreshWebPackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
 
 module.exports = {
   name: "wordrelay-setting",
@@ -18,15 +19,31 @@ module.exports = {
         test: /\.jsx?/,
         loader: "babel-loader",
         options: {
-          presets: ["@babel/preset-env", "@babel/preset-react"],
-          plugins: ["@babel/plugin-proposal-class-properties"],
+          presets: [
+            [
+              "@babel/preset-env",
+              { targets: { browsers: ["> 1% in KR"] }, debug: true },
+            ],
+            "@babel/preset-react",
+          ],
+          plugins: [
+            "@babel/plugin-proposal-class-properties",
+            "react-refresh/babel",
+          ],
         },
       },
     ],
   },
+  plugins: [new ReactRefreshWebPackPlugin()],
 
   output: {
     path: path.join(__dirname, "dist"),
     filename: "app.js",
+    publicPath: "/dist/",
   }, //출력
+  devServer: {
+    devMiddleware: { publicPath: "/dist" },
+    static: { directory: path.resolve(__dirname) },
+    hot: true,
+  },
 };
